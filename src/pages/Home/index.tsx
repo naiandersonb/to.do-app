164d6babec44d73618db181c2ed3c038c8fd80d8
naiandersonb <nayanderson2014@gmail.com>
2023-08-30 17:55:56 -0300

    teste

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,6 +8,8 @@ import { Task } from "../../interfaces/task";
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
+  console.log('teste')
+
   const handleAddTask = useCallback((newTaskTitle: string) => {
     if (!newTaskTitle) return
 
